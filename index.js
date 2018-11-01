d9902fde133dcdcff1f47239ddb7a42b94f23392
Sam Chung <words.sam@gmail.com>
2018-11-01 20:58:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 var shout = (string) => {
   return string.toUpperCase();
 }
+
+var whisper = (string) => {
+  return string.toLowerCase();
+}
+
+var logShout = (string) => {
+  console.log(string.toUpperCase());
+}
+
+var logWhisper = (string) => {
+  console.log(string.toLowerCase());
+}
+
+var sayHiToGrandma = (string) => {
+  return string;
+}
